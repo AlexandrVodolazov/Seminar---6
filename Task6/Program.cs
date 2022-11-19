ea9d6ebf7ac0a0fb6630ba4ab2552c0930ba0b1c
Alexandr Vodolazov <Vodolazov.09@mail.ru>
2022-11-19 19:33:21 +0300

    от 1....5

---
+++ Task6/Program.cs
@@ -0,0 +1,19 @@
+﻿//от 1....5
+
+int Prompt(string msg)
+{
+    Console.Write(msg);
+    int length = Convert.ToInt32(Console.ReadLine());
+    return length;
+}
+
+void Seq(int n)
+{
+    if (n <= 0)
+    {
+        return;
+    }
+    Seq(n - 1);
+    Console.Write($" {n} ");
+}
+Seq(5);
