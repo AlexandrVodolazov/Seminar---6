f46e2aaaa8ac0a3c57b09bc35aff28e6060de968
Alexandr Vodolazov <Vodolazov.09@mail.ru>
2022-11-20 12:53:37 +0300

    H/W

---
+++ Homework/Task1/Program.cs
@@ -0,0 +1,46 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+int PromptInt(string strMessage)
+{
+    Console.Write(strMessage);
+    int temp = int.Parse(Console.ReadLine());
+    return temp;
+}
+
+int[] PromptNumbers(int intN)
+{
+    int[] tempArray = new int[intN];
+    for (int i = 0; i<intN; i++)
+    {
+        Console.Write($"Enter {i+1}-ое number: ");
+        tempArray[i] = int.Parse(Console.ReadLine());
+    }
+    return tempArray;
+}
+
+void printArray(int[] intArray)
+{
+    Console.Write($"{intArray[0]}");
+    for (int i = 1; i < intArray.Length; i++)
+    {
+        Console.Write($", {intArray[i]}");
+    }
+}
+
+void PrintCountOfPositive(int[] intArray)
+{
+    int counter = 0;
+    for (int i = 0; i < intArray.Length; i++)
+    {
+        if (intArray[i] > 0) counter++;
+    }
+    Console.Write("Among the numbers ");
+    printArray(intArray);
+    Console.Write($" - {counter} more numbers 0");
+}
+
+int intN = PromptInt("How many integers are you willing to enter: ");
+int[] arrNumbers = PromptNumbers(intN);
+PrintCountOfPositive(arrNumbers);
---
+++ Homework/Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 2. Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+int PromptInt(string strMessage)
+{
+    Console.Write(strMessage);
+    int temp = int.Parse(Console.ReadLine());
+    return temp;
+}
+
+string Findintersection (int intB1, int intK1, int intB2, int intK2)
+{
+    if (intB1==intB2 && intK1==intK2) return "Lines coincide, an infinite number of common points.";
+    if (intK1==intK2) return "Lines are parallel, no points of intersection.";
+    double dblX =  Math.Round((double)(intB2 - intB1) / (double)(intK1 - intK2),2);
+    double dblY = Math.Round(intK1 * dblX + intB1,2);
+    return $"The lines intersect at a point({dblX}, {dblY}).";
+}
+
+int intB1 = PromptInt("Enter b1: ");
+int intK1 = PromptInt("Enter k1: ");
+int intB2 = PromptInt("Enter b2: ");
+int intK2 = PromptInt("Enter k2: ");
+Console.Write(Findintersection (intB1: intB1, intK1, intB2, intK2));
+
